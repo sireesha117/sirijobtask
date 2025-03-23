db440f38cb69d4e7f9e38e5da95095501f315035
sireesha <pedamallusireesha117@gmail.com>
2025-03-23 12:13:28 +0000

    commit1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css'
-import {Switch, Route} from 'react-router-dom'
-import JobApp from './components/JobApp'
-
-// These are the lists used in the application. You can move them to any component needed.
+import {Switch, Route} from 'react-route-dom'
+import Home from './components/Home'
+import LoginPage from './components/LoginPage'
+import Jobs from './components/Jobs'
+import SpecificJobDetails from './components/SpecificJobDetails'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
 
 // Replace your code here
 const App = () => (
   <>
     <Switch>
       <Route exact to="/" component={Home} />
-      <Route exact to="/login" component={LoginPage} />
-      <Route exact to="/" component={Home} />
+      <ProtectedRoute exact to="/login" component={LoginPage} />
+      <ProtectedRoute exact to="/jobs" component={Jobs} />
+      <ProtectedRoute exact to="/jobs/:id" component={SpecificJobDetails} />
+      <Route component={NotFound} />
     </Switch>
   </>
 )
---
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -0,0 +1,11 @@
+import {Route, Redirect} from 'react-route-com'
+import Cookies from 'js-cookie'
+
+const ProtectedRoute = props => {
+  const jwtToken = Cookies.get('jwt-token')
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+  return <Route {...props} />
+}
+export default ProtectedRoute
