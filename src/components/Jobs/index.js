23c383d465b47f1fb1c0baa43936d27acaf19aae
sireesha <pedamallusireesha117@gmail.com>
2025-03-24 14:29:42 +0000

    dfhjsd

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -1,56 +1,81 @@
 import './index.css'
+import {Component} from 'react'
 import Header from '../Header'
 import GetProfile from '../GetProfile'
+import SelectOptions from '../SelectOptions'
 
-// const employmentTypesList = [
-//   {
-//     label: 'Full Time',
-//     employmentTypeId: 'FULLTIME',
-//   },
-//   {
-//     label: 'Part Time',
-//     employmentTypeId: 'PARTTIME',
-//   },
-//   {
-//     label: 'Freelance',
-//     employmentTypeId: 'FREELANCE',
-//   },
-//   {
-//     label: 'Internship',
-//     employmentTypeId: 'INTERNSHIP',
-//   },
-// ]
+const employmentTypesList = [
+  {
+    label: 'Full Time',
+    employmentTypeId: 'FULLTIME',
+  },
+  {
+    label: 'Part Time',
+    employmentTypeId: 'PARTTIME',
+  },
+  {
+    label: 'Freelance',
+    employmentTypeId: 'FREELANCE',
+  },
+  {
+    label: 'Internship',
+    employmentTypeId: 'INTERNSHIP',
+  },
+]
 
-// const salaryRangesList = [
-//   {
-//     salaryRangeId: '1000000',
-//     label: '10 LPA and above',
-//   },
-//   {
-//     salaryRangeId: '2000000',
-//     label: '20 LPA and above',
-//   },
-//   {
-//     salaryRangeId: '3000000',
-//     label: '30 LPA and above',
-//   },
-//   {
-//     salaryRangeId: '4000000',
-//     label: '40 LPA and above',
-//   },
-// ]
-const Jobs = () => (
-  <div>
-    <Header />
-    <div className="jobtab">
-      <div className="left">
-        <GetProfile />
+const salaryRangesList = [
+  {
+    salaryRangeId: '1000000',
+    label: '10 LPA and above',
+  },
+  {
+    salaryRangeId: '2000000',
+    label: '20 LPA and above',
+  },
+  {
+    salaryRangeId: '3000000',
+    label: '30 LPA and above',
+  },
+  {
+    salaryRangeId: '4000000',
+    label: '40 LPA and above',
+  },
+]
+
+const apiSts = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'Failure',
+  inprogress: 'INPROGRESS',
+}
+
+class Jobs extends Component {
+  state = {
+    checkBox: '',
+    radioInput: '',
+    searchInput: '',
+    apiStsData: apiSts.initial,
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="jobtab">
+          <div className="left">
+            <GetProfile />
+            <SelectOptions
+              employmentTypesList={employmentTypesList}
+              salaryRangesList={salaryRangesList}
+            />
+          </div>
+          <div className="right">
+            <p>right</p>
+          </div>
+        </div>
       </div>
-      <div className="right">
-        <p>right</p>
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 export default Jobs
